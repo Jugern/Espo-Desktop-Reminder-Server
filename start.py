ab4487f147c5f3daa347bad7583632488aa77153
Jugern <nikpox@yandex.ru>
2022-09-05 00:08:01 +0300

    started from the beginning

--- start.py
+++ start.py
@@ -2,74 +2,54 @@ import mariadb
 import os
 from dotenv import load_dotenv
 
+class Connections():
+    def connect(self, dataBasa, commandData):
+        try:
+            dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+            if os.path.exists(dotenv_path):
+                load_dotenv(dotenv_path)
+            self.conn_params = {
+                "user": os.environ.get('user'),
+                "password": os.environ.get('password'),
+                "host": os.environ.get('host'),
+                "database": os.environ.get(f'{dataBasa}')
+            }
+            zapros = (self.conn_params)
+            connection = mariadb.connect(**zapros)
+            cursor = connection.cursor()
+            cursor.execute(f"""{commandData}""")
+            row = cursor.fetchall()
+            cursor.close()
+            connection.close()
+            return row
+        except:
+            print('error')
 
-class Notification():
-    def __init__(self):
-        dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-        if os.path.exists(dotenv_path):
-            load_dotenv(dotenv_path)
-        self.conn_params = {
-            "user": os.environ.get('user'),
-            "password": os.environ.get('password'),
-            "host": os.environ.get('host'),
-            "database": os.environ.get('database')
-        }
-    def connect(self):
-        zapros = (self.conn_params)
-        connection = mariadb.connect(**zapros)
-        cursor = connection.cursor()
-        cursor.execute(f"""
-        SELECT reminder.id, reminder.remind_at, reminder.entity_type, user.user_name
-        FROM reminder
-        JOIN user ON reminder.user_id = user.id
-        WHERE reminder.deleted != 1""")
-        row = cursor.fetchall()
-        # print(row)
-        # c = (row)
-        # print(*c, ' ')
-        cursor.close()
-        connection.close()
-        return row
-
-class Data():
-    def __init__(self):
-        dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-        if os.path.exists(dotenv_path):
-            load_dotenv(dotenv_path)
-        self.conn_params = {
-            "user": os.environ.get('user'),
-            "password": os.environ.get('password'),
-            "host": os.environ.get('host'),
-            "database": os.environ.get('databaseTwo')
-        }
-
-    def sravnenie(self, basa):
-        result1 = list(set(basa) - set(self.row))
-        result2 = list(set(self.row) - set(basa))
+class Data(Connections):
+    def sravnenie(self, row, basa):
+        result1 = list(set(basa) - set(row))
+        result2 = list(set(row) - set(basa))
         print(result1)
         print(result2)
 
-    def addData(self, basa):
-        zapros = (self.conn_params)
-        connection = mariadb.connect(**zapros)
-        cursor = connection.cursor()
-        cursor.execute(f"""
-        SELECT id, data, class, user
-        FROM notifications""")
-        self.row = cursor.fetchall()
-        cursor.close()
-        connection.close()
-        self.sravnenie(basa)
+    def addData(self, basa, database, commandMysql):
+        try:
+            row = self.connect(database, commandMysql)
+            self.sravnenie(row, basa)
+        except:
+            print('error')
 
+mySqlCommandProverka = """
+            SELECT reminder.id, reminder.remind_at, reminder.entity_type, user.user_name
+            FROM reminder
+            JOIN user ON reminder.user_id = user.id
+            WHERE reminder.deleted != 1"""
 
+mySqlCommandSozdanie = """
+            SELECT id, data, class, user
+            FROM notifications"""
 
-
-zapros = Notification()
-basa = zapros.connect()
+zapros = Connections()
+basa = zapros.connect('database', mySqlCommandProverka)
 zapros = Data()
-zapros.addData(basa)
-
-
-
-
-
+zapros.addData(basa, 'databaseTwo', mySqlCommandSozdanie)
