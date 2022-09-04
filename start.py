d6fbe5304a70be0e251ebff76e6ed03e6f232bbe
Jugern <nikpox@yandex.ru>
2022-09-05 01:16:59 +0300

    reversed project

--- start.py
+++ start.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 
 class Connections():
-    def connect(self, dataBasa, commandData):
+    def connect(self, dataBasa, commandData, soed):
         try:
             dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
             if os.path.exists(dotenv_path):
@@ -17,25 +17,43 @@ class Connections():
             zapros = (self.conn_params)
             connection = mariadb.connect(**zapros)
             cursor = connection.cursor()
-            cursor.execute(f"""{commandData}""")
-            row = cursor.fetchall()
-            cursor.close()
-            connection.close()
-            return row
+            # print(commandData)
+            # print(soed)
+            if soed == False:
+                cursor.execute(f"""{commandData}""")
+                row = cursor.fetchall()
+                cursor.close()
+                connection.close()
+                # print(row)
+                return row
+            if soed:
+                cursor.execute(f"""{commandData}""", soed)
+                connection.commit()
+                cursor.close()
+                connection.close()
+                # print(soed)
+                return True
         except:
             print('error')
 
 class Data(Connections):
-    def sravnenie(self, row, basa):
+    def sravnenie(self, row, basa, database):
         result1 = list(set(basa) - set(row))
         result2 = list(set(row) - set(basa))
+        for i in result1:
+            commandi = (f"INSERT INTO notifications(notification_id, data, class, user) VALUES (?,?,?,?)")
+            self.connect(database, commandi, i)
+        for i in result2:
+            commandi = (f"DELETE FROM notifications WHERE notification_id=?")
+            turle = (i[0],)
+            self.connect(database, commandi, turle)
         print(result1)
         print(result2)
 
-    def addData(self, basa, database, commandMysql):
+    def addData(self, basa, database, commandMysql, soed):
         try:
-            row = self.connect(database, commandMysql)
-            self.sravnenie(row, basa)
+            row = self.connect(database, commandMysql, soed)
+            self.sravnenie(row, basa, database)
         except:
             print('error')
 
@@ -46,10 +64,10 @@ mySqlCommandProverka = """
             WHERE reminder.deleted != 1"""
 
 mySqlCommandSozdanie = """
-            SELECT id, data, class, user
+            SELECT notification_id, data, class, user
             FROM notifications"""
 
 zapros = Connections()
-basa = zapros.connect('database', mySqlCommandProverka)
+basa = zapros.connect('database', mySqlCommandProverka, False)
 zapros = Data()
-zapros.addData(basa, 'databaseTwo', mySqlCommandSozdanie)
+zapros.addData(basa, 'databaseTwo', mySqlCommandSozdanie, False)
